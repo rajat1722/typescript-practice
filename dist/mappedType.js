e5f4c2ff41848e74bac11ecc6331cefeeb38b2fc
Rajat Rana <rajat.rana@vidyamantra.com>
2022-10-26 17:20:10 +0530

    mapped type

---
+++ dist/mappedType.js
@@ -0,0 +1,9 @@
+"use strict";
+const check = {
+    x: 1,
+    y: 2
+};
+const check1 = {
+    x: 1,
+    y: 2
+};
---
+++ src/mappedType.ts
@@ -0,0 +1,30 @@
+type Point = {
+    x: number
+    y: number
+}
+
+type ReadOnly<Type> = {
+    readonly [key in keyof Type]: Type[key]
+}
+
+const check: ReadOnly<Point> = {
+    x: 1, 
+    y: 2
+}
+
+
+// modifiers
+
+type Point1 = {
+    readonly x: number
+    y?: number
+}
+
+type NotOptionalY<Type> = {
+    [key in keyof Point1] -?: Point1[key]
+}
+
+const check1: NotOptionalY<Point1> = {
+    x: 1,
+    y: 2
+}
